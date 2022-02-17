1b1dc371a154cfadee536ee8b54f766fea2ce75f
elchuudeleon <jesus.deleon04@hotmail.com>
2022-02-16 22:54:47 -0500

    version para trabajo en local

--- php/globals.php
+++ php/globals.php
@@ -1,9 +1,12 @@
 <?php 
+error_reporting(E_ALL);
+ini_set('display_errors', 'true');
+ini_set('display_startup_errors', 'true');
 global $DEVELOP_ENVIRONMENT,$CLASS,$GLOBALS,$THEMES, $FILES, $ROOT, $URL,$DB,$SERVER,$USERDB,$PWDDB;
 
 $DEVELOP_ENVIRONMENT = false;
-$ROOT = $_SERVER["DOCUMENT_ROOT"]."/";	
-$URL = "https://smartbuss.co/";
+$ROOT = $_SERVER["DOCUMENT_ROOT"]."/juriscon/";	
+$URL = "http://localhost/juriscon/";
 
 $GLOBALS = $ROOT ."php/";
 $CLASS = $ROOT ."models/";
